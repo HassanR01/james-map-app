19c2e325174df22d4322afcc291b7ddc6fd4f1de
hassanr01 <hassanrageh.236@gmail.com>
2025-01-16 21:44:09 +0200

    push updates

--- components/context/DataContext.tsx
+++ components/context/DataContext.tsx
@@ -148,11 +148,11 @@ export default function DataProvider({ children }: { children: ReactNode }) {
         const fetchData = async () => {
             try {
                 const [resDev, resUnits, resProjects, resZones, resUsers] = await Promise.all([
-                    axios.get<Developer[]>('http://192.168.1.41:5000/api/v1/developers/get-developers'),
-                    axios.get<Unit[]>('http://192.168.1.41:5000/api/v1/units/get-units'),
-                    axios.get<Project[]>('http://192.168.1.41:5000/api/v1/projects/get-projects'),
-                    axios.get<Zone[]>('http://192.168.1.41:5000/api/v1/zones/get-zones'),
-                    axios.get<User[]>('http://192.168.1.41:5000/api/v1/users/get-users'),
+                    axios.get<Developer[]>('http://172.20.10.2:5000/api/v1/developers/get-developers'),
+                    axios.get<Unit[]>('http://172.20.10.2:5000/api/v1/units/get-units'),
+                    axios.get<Project[]>('http://172.20.10.2:5000/api/v1/projects/get-projects'),
+                    axios.get<Zone[]>('http://172.20.10.2:5000/api/v1/zones/get-zones'),
+                    axios.get<User[]>('http://172.20.10.2:5000/api/v1/users/get-users'),
                 ]);
 
                 setDevelopers(resDev.data);
